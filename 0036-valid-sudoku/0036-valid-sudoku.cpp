5902c3a6879b8d77c213a7196eb51e4f3db4ce77
Jia Ze Chong <106980570+JiaZe-Chong@users.noreply.github.com>
2023-08-19 23:22:09 -0500

    Time: 11 ms (96.66%), Space: 19.8 MB (38.49%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        
+        vector<map<int,bool>> vmv(9);
+        vector<map<int,bool>> vmh(9);
+        
+        for (int i = 0; i < 9; i++){
+            for (int j = 0; j < 9; j++){
+                if ( board[i][j] == '.' ) continue;
+                
+                int k = board[i][j] - '0';
+                if ( vmv[i][k] ) return false;
+                if ( vmh[j][k] ) return false;
+                vmv[i][k] = vmh[j][k] = true;
+            }
+        }
+        
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                map<int, bool> m;
+                for (int k = 0; k < 3; k++){
+                    for (int l = 0; l < 3; l++){
+                        if ( board[i * 3 + k][j * 3 + l] == '.' ) continue;
+                        cout << board[i * 3 + k][j * 3 + l] - '0';
+                        int temp = board[i * 3 + k][j * 3 + l] - '0';
+                        if ( m[temp] ) return false;
+                        m[temp] = true;
+                    }
+                }
+                cout << endl;
+            }
+        }
+        
+        return true;
+    }
+};
