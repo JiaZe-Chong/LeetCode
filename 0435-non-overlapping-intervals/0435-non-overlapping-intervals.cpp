1abfd42c066f8eb6c53817c8010a734e3fdfb7c9
Jia Ze Chong <106980570+JiaZe-Chong@users.noreply.github.com>
2023-09-06 22:36:40 -0500

    Time: 302 ms (98.52%), Space: 89.8 MB (28.16%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        
+        int n = intervals.size();
+        if ( n == 1 ) return 0;
+        
+        sort( intervals.begin(), intervals.end(), []( const auto& a, const auto& b ) {
+            return a[0] < b[0];
+        } );
+        
+        int ret = 0;
+        
+        int i = 0;
+        while ( i < n - 1 ) {
+            
+            if ( intervals[i][1] > intervals[i + 1][0] ) {
+                
+                if ( intervals[i][1] < intervals[i + 1][1] ) {
+                    
+                    intervals[i + 1] = intervals[i];
+                    
+                }
+                
+                ret++;
+                
+            }
+            
+            i++;
+            
+        }
+        
+        return ret;
+        
+    }
+};
