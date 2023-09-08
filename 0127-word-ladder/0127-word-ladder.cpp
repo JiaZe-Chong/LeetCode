db0ca5d66c817c85709fff18405fd1073a7596ad
Jia Ze Chong <106980570+JiaZe-Chong@users.noreply.github.com>
2023-09-07 19:52:12 -0500

    Time: 63 ms (91.81%), Space: 15.5 MB (53.03%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> us;
+        for ( string s : wordList ) us.insert( s );
+        
+        queue<string> q;
+        q.push( beginWord );
+        int result = 1;
+        
+        while( !q.empty() ) {
+            int n = q.size();
+            
+            while ( n-- ) {
+                string cur = q.front();
+                q.pop();
+                
+                if ( cur == endWord ) return result;
+                us.erase( cur );
+                
+                for (int i = 0 ; i < cur.length(); i++){
+                    char c = cur[i];
+                    for (int j = 0; j < 26; j++){
+                        cur[i] = 'a' + j;
+                        if ( us.find( cur ) != us.end() ) {
+                            q.push( cur );
+                            us.erase( cur );
+                        }
+                    }
+                    cur[i] = c;
+                }
+
+            }
+            
+            result++;
+        }
+        
+        return 0;
+    }
+};
