c29774410e0fb30359a542446250bbebe13aa1bb
Jia Ze Chong <106980570+JiaZe-Chong@users.noreply.github.com>
2024-03-03 16:38:18 -0600

    Time: 253 ms (61.03%), Space: 93.4 MB (27.93%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -5,14 +5,11 @@ public:
         int num = 0;
         
         for (int i = 0; i < intervals.size() - 1; i++) {
-            
             if (intervals[i][1] > intervals[i + 1][0]) {
-                
                 if (intervals[i + 1][1] > intervals[i][1])
                     intervals[i + 1] = intervals[i];                
                 num++;
             }
-
         }
         
         return num;
