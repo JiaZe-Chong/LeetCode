1d67532232866362c0a80feb60b1beb31fe7648a
Jia Ze Chong <106980570+JiaZe-Chong@users.noreply.github.com>
2024-03-03 16:01:24 -0600

    Time: 27 ms (48.42%), Space: 23.6 MB (19.38%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,37 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> ans;
         
-        int n = intervals.size();
-        if ( n == 1 ) return intervals;
-        
-        sort( intervals.begin(), intervals.end(), [](const auto& a, const auto& b) {
-            return a[0] < b[0];
-        } );
-        
-        vector<vector<int>> ret;
-        
-        int i = 0;
-        while ( i < n - 1 ) {
-            
-            if ( intervals[i][1] >= intervals[i + 1][0] ) {
-                
-                intervals[i + 1][0] = intervals[i][0];
-                intervals[i + 1][1] = max( intervals[i + 1][1], intervals[i][1] );
-                
+        for (auto interval : intervals) {
+            if (!ans.size() || interval[0] > ans[ans.size() - 1][1]) {
+                ans.push_back(interval);
             }
-            else {
-                
-                ret.push_back( intervals[i] );
-                
+            else if (interval[0] <= ans[ans.size() - 1][1]) {
+                ans[ans.size() - 1][1] = max(ans[ans.size() - 1][1], interval[1]);
             }
-            
-            i++;    
-            
         }
-        ret.push_back( intervals[i] );
         
-        return ret;
-        
+        return ans;
     }
 };
