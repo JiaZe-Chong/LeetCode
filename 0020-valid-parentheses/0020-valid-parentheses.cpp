6acd70025fff01b3b6014283330a76bc9ece6b9e
Jia Ze Chong <106980570+JiaZe-Chong@users.noreply.github.com>
2024-09-20 16:24:43 -0500

    Time: 0 ms (100.00%), Space: 7.8 MB (82.92%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
     bool isValid(string s) {
-        
         stack<char> stk;
         
-        for (int i = 0; i < s.length(); i++){
-            char c = s[i];
-            if (c == '(' || c == '[' || c=='{') stk.push(c);
-            else{
-                if (stk.empty()) return false;
-                char temp = stk.top();
-                if (temp == '[' && c == ']') stk.pop();
-                else if (temp == '(' && c == ')') stk.pop();
-                else if (temp == '{' && c == '}') stk.pop();
-                else return false;
+        for (char c : s) {
+            
+            if (c == '(' || c == '{' || c == '[')
+                stk.push(c);
+            
+            else {
+                if (stk.empty())
+                    return false;
+                
+                if (c == ')') {
+                    if (stk.top() == '(')
+                        stk.pop();
+                    else 
+                        return false;
+                }
+
+                
+                if (c == ']') {
+                    if (stk.top() == '[')
+                        stk.pop();
+                    else 
+                        return false;
+                }
+                
+                if (c == '}') {
+                    if (stk.top() == '{')
+                        stk.pop();
+                    else 
+                        return false;
+                }
+                
             }
             
-            
         }
+        
         return stk.empty();
     }
 };
