04de57f5e820ff7023f6581f37af541e2fed4c6f
Jia Ze Chong <106980570+JiaZe-Chong@users.noreply.github.com>
2024-09-26 11:33:20 -0500

    Time: 98 ms (86.55%), Space: 82.3 MB (60.74%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -5,25 +5,29 @@ public:
         int ret = 0;
 
         for (int i = 0; i < heights.size(); i++) {
+
             int start = i;
 
             while ( !sp.empty() && sp.top().second > heights[i] ) {
-                const auto& [ind, height] = sp.top();
-                int width = i - ind;
+
+                int ind = sp.top().first;
+                int height = sp.top().second;
                 sp.pop();
 
+                int width = sp.empty() ? i : i - sp.top().first - 1;
                 ret = max( ret, width * height );
-                start = ind;
+
             }
 
             sp.push( {start, heights[i]} );
+
         }
 
         while ( !sp.empty() ) {
-            int width = heights.size() - sp.top().first;
             int height = sp.top().second;
             sp.pop();
 
+            int width = sp.empty() ? heights.size() : heights.size() - sp.top().first - 1;
             ret = max( ret, width * height );
         }
 
