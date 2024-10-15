b5ee1dd842c84eee0f0060fcde06bafd940540a7
Jia Ze Chong <106980570+JiaZe-Chong@users.noreply.github.com>
2024-10-15 11:44:02 -0500

    Time: 19 ms (69.46%), Space: 27.6 MB (50.25%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-        k = k % n;
-        
-        vector<int> ans(n);
-        
-        memcpy(&(ans[0]), &(nums[n - k]), k * sizeof(int));   
-        memcpy(&(ans[k]), &(nums[0]), (n - k) * sizeof(int));   
-        
-        nums = ans;
-    }
+void rotate(vector<int>& nums, int k) {
+    int n = nums.size();
+    k = k % n;
+    if (k == 0)
+        return;
+    
+    reverse(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.begin() + k);
+    reverse(nums.begin() + k, nums.end());
+}
 };
