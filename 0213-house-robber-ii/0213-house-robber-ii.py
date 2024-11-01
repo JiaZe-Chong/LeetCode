886987c86a695f75185ed77b7ad01893011a3936
Jia Ze Chong <106980570+JiaZe-Chong@users.noreply.github.com>
2024-11-01 13:26:32 -0500

    Time: 0 ms (100.00%), Space: 11.5 MB (91.05%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -4,25 +4,36 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if not nums:
-            return 0
+        def rob_linear(houses):
+            # Base cases for no houses and a single house, respectively
+            if not houses:
+                return 0
+            if len(houses) == 1:
+                return houses[0]
+            # Create a DP array where dp[i] will be the max amount that can be robbed up to house i
+            dp = [0] * len(houses)
+            dp[0] = houses[0]
+            dp[1] = max(houses[0], houses[1])
+
+            # Fill in the DP table
+            for i in range(2, len(houses)):
+                # Take the maximum of robbing the current house plus the max value two houses back,
+                # or not robbing the current house and just taking the max value from the previous house
+                dp[i] = max(dp[i-1], dp[i-2] + houses[i])
+
+            # The max value at the last house is the result
+            return dp[-1]
+
+        # Edge case: If there's only one house, we can rob it without worry
         if len(nums) == 1:
             return nums[0]
 
-        # A helper function to do the robbing for a linear street of houses
-        def rob_linear(houses):
-            prev1, prev2 = 0, 0
-            for money in houses:
-                temp = prev1
-                prev1 = max(prev1, prev2 + money)
-                prev2 = temp
-            return prev1
+        # Use rob_linear function to calculate the max money we can rob in each sub-problem
+        # Rob houses from `0 to n-2`
+        money1 = rob_linear(nums[:-1])
+        # Rob houses from `1 to n-1`
+        money2 = rob_linear(nums[1:])
 
-        # Case 1: Rob houses from 0 to n-2
-        case1 = rob_linear(nums[:-1])
-        # Case 2: Rob houses from 1 to n-1
-        case2 = rob_linear(nums[1:])
-
-        # Return the maximum of the two cases
-        return max(case1, case2)
+        # Return the maximum money of the two linear sub-problems
+        return max(money1, money2)
         
