9ea299cdfcd7ec82c5e1e5a743e8923682db15ca
Jia Ze Chong <106980570+JiaZe-Chong@users.noreply.github.com>
2024-11-03 10:21:36 -0600

    Time: 0 ms (100.00%), Space: 8.7 MB (67.80%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -3,8 +3,8 @@ public:
     string gcdOfStrings(string str1, string str2) {
         if (str1 + str2 != str2 + str1)
             return "";
-            
-        int l = gcd(str1.length(), str2.length());
-        return str1.substr(0, l);
+        
+        int len = gcd(str1.length(), str2.length());
+        return str1.substr(0, len);
     }
 };
