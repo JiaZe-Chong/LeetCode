008d50f1fb89fc206d8df4da60f9959e1263e404
Jia Ze Chong <106980570+JiaZe-Chong@users.noreply.github.com>
2024-11-03 13:17:24 -0600

    Time: 0 ms (100.00%), Space: 36 MB (15.26%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         int n = nums.size();
-        vector<int> left(n);
-        vector<int> right(n);
-        left[0] = 0;
-        right[0] = 0;
+        vector<int> left(n, 0);
+        vector<int> right(n, 0);
         
-        for (int i = 1; i < n; i++) left[i] = left[i - 1] + nums[i - 1];
-        for (int i = 1; i < n; i++) right[i] = right[i - 1] + nums[n - i];
+        for (int i = 1; i < n; i++) {
+            left[i] = left[i - 1] + nums[i - 1];
+        }
         
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = right[i + 1] + nums[i + 1];
+        }
         
-        for (int i = 0; i < n; i++)
-            if (left[i] == right[n - i - 1]) return i;
-        
-        // for (int i = 0; i < n; i++) cout << left[i] << " ";
-        // cout << endl;
-        // for (int i = 0; i < n; i++) cout << right[i] << " ";
+        for (int i = 0; i < n; i++) {
+            if (left[i] == right[i])
+                return i;
+        }
         
         return -1;
     }
