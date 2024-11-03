41f7cd3d7119e86e91f1c2dd1e1121fc72bbaf33
Jia Ze Chong <106980570+JiaZe-Chong@users.noreply.github.com>
2024-11-03 10:58:59 -0600

    Time: 0 ms (100.00%), Space: 14.1 MB (33.76%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
+        int ans = 0;
         int len = 0;
-        vector<char> ret;
-        
         char cur = chars[0];
-        int num = 0;
-        for(char c : chars) {
-            if (c == cur) num++;
-            else{
-                ret.push_back(cur);
-                if (num > 1) {
-                    string n = to_string(num);
-                    for (auto a : n) ret.push_back(a);   
+        
+        for (const char& c : chars) {
+            if (c == cur) {
+                len++;
+            }
+            else {
+                chars[ans++] = cur;
+                if (len != 1) {
+                    string num = to_string(len);
+                    for (const char& n : num) {
+                        chars[ans++] = n;
+                    }   
                 }
                 cur = c;
-                num = 1;
-            }
-        }
-        ret.push_back(cur);
-        if (num > 1) {
-            string n = to_string(num);
-            for (auto a : n) ret.push_back(a);   
+                len = 1;
+            }        
         }
         
-        chars = ret;
-        return ret.size();
+        chars[ans++] = cur;
+        if (len != 1) {
+            string num = to_string(len);
+            for (const char& n : num) {
+                chars[ans++] = n;
+            }   
+        }
+        
+        return ans;
     }
 };
