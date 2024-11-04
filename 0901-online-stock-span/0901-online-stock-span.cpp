ee2b3c6ee1cb7c7f44ab7708597b766218ae1826
Jia Ze Chong <106980570+JiaZe-Chong@users.noreply.github.com>
2024-11-04 16:28:38 -0600

    Time: 33 ms (58.81%), Space: 90.5 MB (37.99%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -5,19 +5,19 @@ public:
     }
     
     int next(int price) {
-        
         int span = 1;
-        while( stk.size() && stk.top().first <= price ) {
-            span += stk.top().second;
+        
+        while(stk.size() && stk.top().first <= price) {
+            const auto& [_, s] = stk.top();
+            span += s;
             stk.pop();
         }
         stk.push({price, span});
-        return span;
         
+        return span;
     }
     
     stack<pair<int, int>> stk;
-    
 };
 
 /**
