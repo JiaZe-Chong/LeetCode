dac906b1158a449dfa78a4661227ac3b1b478447
Jia Ze Chong <106980570+JiaZe-Chong@users.noreply.github.com>
2024-11-05 16:59:45 -0600

    Time: 123 ms (72.33%), Space: 115.1 MB (55.15%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     int minReorder(int n, vector<vector<int>>& connections) {
         vector<vector<pair<int, int>>> adj(n);
-        for (const auto& connection : connections) {
+        for (auto connection : connections) {
             adj[connection[0]].push_back({connection[1], 1});
             adj[connection[1]].push_back({connection[0], 0});
         }
         return dfs(0, -1, adj);
     }
     
-    int dfs(int node, int parent, const vector<vector<pair<int, int>>>& adj) {
-        int ret = 0;
-        for (auto& [neighbor, sign] : adj[node]) {
-            if (neighbor != parent) {
-                ret += sign;
-                ret += dfs(neighbor, node, adj);
+    int dfs(int cur, int from, const vector<vector<pair<int, int>>>& adj) {
+        int ans = 0;
+        for (const auto& [to, cost] : adj[cur]) {
+            if (to != from) {
+                ans += dfs(to, cur, adj);
+                ans += cost;
             }
         }
-        return ret;
+        return ans;
     }
 };
