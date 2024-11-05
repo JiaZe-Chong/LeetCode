a18557819ae9afd901217f626399e94dfd975ad7
Jia Ze Chong <106980570+JiaZe-Chong@users.noreply.github.com>
2024-11-05 17:18:18 -0600

    Time: 0 ms (100.00%), Space: 10.8 MB (93.20%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.cpp
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -2,36 +2,39 @@ class Solution {
 public:
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
         int n = values.size();
-        vector<double> ans;
         unordered_map<string, vector<pair<string, double>>> eval;
         
         for (int i = 0; i < n; i++) {
-            string a = equations[i][0];
-            string b = equations[i][1];
-            double val = values[i];
+            string top = equations[i][0];
+            string bottom = equations[i][1];
+            double value = values[i];
             
-            eval[a].push_back( {b, val} );
-            eval[b].push_back( {a, 1 / val} );
+            eval[top].push_back({bottom, value});
+            eval[bottom].push_back({top, 1 / value});
         }
         
-        for (auto query : queries) {
-            unordered_set<string> visited;
-            double res = dfs( eval, query[0], query[1], visited );
-            ans.push_back( res );
+        vector<double> ans;
+        for (const auto& query : queries) {
+            set<string> visited;
+            ans.push_back(dfs(eval, query[0], query[1], visited));
         }
-        
         return ans;
     }
     
-    double dfs(unordered_map<string, vector<pair<string, double>>>& eval, string src, string des, unordered_set<string>& visited) {
-        if (!eval.contains(src)) return -1;
-        if (src == des) return 1;
+    double dfs(unordered_map<string, vector<pair<string, double>>>& eval, string top, string bottom, set<string>& visited) {
+        if (!eval.contains(top) || !eval.contains(bottom))
+            return -1;
         
-        for (auto node : eval[src]) {
-            if (visited.contains(node.first)) continue;
-            visited.insert(node.first);
-            double res = dfs(eval, node.first, des, visited);
-            if (res != -1) return res * node.second;
+        if (top == bottom)
+            return 1;
+        
+        for (const auto& [str, value] : eval[top]) {
+            if (visited.contains(str))
+                continue;
+            visited.insert(str);
+            double res = dfs(eval, str, bottom, visited);
+            if (res != -1)
+                return value * res;
         }
         
         return -1;
